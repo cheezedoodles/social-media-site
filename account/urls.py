954119a2602ea46196f9b022732cdba8988de169
cheezedoodles <hougvapes@gmail.com>
2022-06-21 18:02:44 +0300

    now you can change passwords

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView,\
+    PasswordChangeView, PasswordChangeDoneView
 from . import views
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'),
+    path('password_change/',
+         PasswordChangeView.as_view(),
+         name='password_change'),
+    path('password_change/done/',
+         PasswordChangeDoneView.as_view(),
+         name='password_change_done'),
 ]
