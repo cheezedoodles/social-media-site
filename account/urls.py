04ab51d4c2ee14aac87b7d320773cd10fdd6449c
cheezedoodles <hougvapes@gmail.com>
2022-06-23 19:58:45 +0300

    added profiles

--- account/urls.py
+++ account/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('', include('django.contrib.auth.urls')),
     path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
     # authentication urls
     #path('login/', auth_views.LoginView.as_view(), name='login'),
     #path('logout/', auth_views.LogoutView.as_view(), name='logout'),
