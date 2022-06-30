610cc794812f23419e2fb7db2e1981c95bd8ac9c
cheezedoodles <hougvapes@gmail.com>
2022-06-30 22:42:56 +0300

    added success and error messages

--- account/views.py
+++ account/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+
 from .forms import LoginForm, UserRegistrationForm, \
                    UserEditForm, ProfileEditForm
 from .models import Profile
@@ -61,6 +63,9 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request, 'Error updating your profile')    
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(
