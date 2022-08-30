b68672b68a968d6cf5970144f71edc5b06abfca6
cheezedoodles <hougvapes@gmail.com>
2022-08-30 23:29:34 +0300

    trying out denormalization

--- images/apps.py
+++ images/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class ImagesConfig(AppConfig):
     name = 'images'
+
+    def ready(self):
+        # recommended way of importing signal handlers
+        import images.signals
